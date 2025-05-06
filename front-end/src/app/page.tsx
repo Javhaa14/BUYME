c48373ffed7ee404ca5539cc042d19fbd0c5a101
Javkhlan Battulga <Javkhlan.B@ckbank.mn>
2025-05-06 14:03:36 +0800

    step2

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,9 +1,10 @@
 import { Step1 } from "./components/Step1";
+import { Step2 } from "./components/Step2";
 
 export default function Home() {
   return (
     <div className="bg-white w-full h-screen flex justify-center items-center">
-      <Step1></Step1>
+      <Step2 />
     </div>
   );
 }
