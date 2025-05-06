dc92d1821165113f2251491d08ee1e64f0db0c0e
Javkhlan Battulga <Javkhlan.B@ckbank.mn>
2025-05-06 14:12:00 +0800

    step3

--- front-end/src/app/components/Step2.tsx
+++ front-end/src/app/components/Step2.tsx
@@ -9,7 +9,7 @@ export const Step2 = ({ username }: { username: string }) => {
           Connect email and set a password
         </p>
       </div>
-      <div className="flex px-6 pb-6 flex-col items-start gap-[12px] self-stretch">
+      <div className="text-[#09090B] flex px-6 pb-6 flex-col items-start gap-[12px] self-stretch">
         <div className="flex flex-col items-start gap-[8px] self-stretch">
           <p className="text-[#09090B] text-[14px]">Email</p>
           <input
@@ -17,9 +17,10 @@ export const Step2 = ({ username }: { username: string }) => {
             className="placeholder:text-[#71717A] flex h-[40px] px-3 py-2 items-center self-stretch rounded-md border-[1px] border-[#E4E4E7] bg-white"
           ></input>
         </div>
-        <div className="flex flex-col items-start gap-[8px] self-stretch">
+        <div className="text-[#09090B] flex flex-col items-start gap-[8px] self-stretch">
           <p className="text-[#09090B] text-[14px]">Password</p>
           <input
+            type="password"
             placeholder="Enter password"
             className="placeholder:text-[#71717A] flex h-[40px] px-3 py-2 items-center self-stretch rounded-md border-[1px] border-[#E4E4E7] bg-white"
           ></input>
