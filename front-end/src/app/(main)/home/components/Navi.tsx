0826315f4d47a07f48b4572e00a924db62b39fc9
24LP1634 24LP1634 <24LP1634@G302-CC0108.local>
2025-05-07 11:17:42 +0800

    home comp

--- front-end/src/app/(main)/home/components/Navi.tsx
+++ front-end/src/app/(main)/home/components/Navi.tsx
@@ -3,19 +3,20 @@ import { SquareArrowOutUpRight } from "lucide-react";
 export const Navi = () => {
   const buttons = ["Home", "Explore", "View page", "Account settings"];
   return (
-    <div className="flex z-50 absolute top-[14%] left-[7%] w-[251px] flex-col items-start gap-1">
+    <div className="flex ml-[60px] w-[251px] flex-col items-start gap-1">
       {buttons.map((v, i) => {
         return i == 2 ? (
           <button
             key={i}
-            className="flex w-[250px] h-[36px] px-4 py-2 items-center gap-2 rounded-md text-[#18181B] text-[14px] font-medium">
+            className="flex bg-[#F4F4F5]
+ w-[200px] h-[36px] px-4 py-2 items-center gap-2 rounded-md text-[#18181B] text-[14px] font-medium">
             {v}
             <SquareArrowOutUpRight className="size-4 text-[#18181B]" />
           </button>
         ) : (
           <button
             key={i}
-            className="flex w-[250px] h-[36px] px-4 py-2 items-center gap-2 rounded-md text-[#18181B] text-[14px] font-medium">
+            className="flex w-[200px] h-[36px] px-4 py-2 items-center gap-2 rounded-md text-[#18181B] text-[14px] font-medium">
             {v}
           </button>
         );
@@ -23,4 +24,3 @@ export const Navi = () => {
     </div>
   );
 };
-// bg-[#F4F4F5]
